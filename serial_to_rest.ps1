d3d94b49f9bf86e988c294be1702d4b487c4c818
balejosg <balejosg@redhat.com>
2023-04-26 18:19:14 +0200

    The previous PS version theorically improved by Chatgpt didn't work out

--- serial_to_rest.ps1
+++ serial_to_rest.ps1
@@ -8,38 +8,25 @@ $endpointUrl = "https://localhost:3000/arrivals" # Replace with your REST endpoi
 
 # Open serial port
 $serialPort = New-Object System.IO.Ports.SerialPort -ArgumentList @($portName, $baudRate, $parity, $dataBits, $stopBits)
-$serialPort.ReadTimeout = 1000
 $serialPort.Open()
 
-# Function to send color to the endpoint
+
+
 function SendColorToEndpoint($color) {
     $body = @{
         color = $color
     } | ConvertTo-Json
 
     try {
-        $response = Invoke-WebRequest -Uri $endpointUrl -Method POST -ContentType "application/json" -Body $body
+        $response = Invoke-WebRequest -Uri "http://localhost:3000/arrivals" -Method POST -ContentType "application/json" -Body $body
         Write-Host "Sent color: $color - Response: $($response.StatusCode) $($response.StatusDescription)"
-    } catch {
+    }catch {
         Write-Host "Error sending color: $color - $($_.Exception.Message)"
     }
 }
 
-try {
-    while ($true) {
-        try {
-            $color = $serialPort.ReadLine().Trim()
-            SendColorToEndpoint($color)
-        } catch [System.TimeoutException] {
-            Write-Host "Read timeout, continuing..."
-        } catch {
-            Write-Host "Error reading data from the serial port: $($_.Exception.Message)"
-            break
-        }
-    }
-} finally {
-    if ($serialPort.IsOpen) {
-        $serialPort.Close()
-        Write-Host "Serial port closed."
-    }
+do {
+    $color = $serialPort.ReadLine().Trim()
+    SendColorToEndpoint($color)
 }
+while ($serialPort.IsOpen)
